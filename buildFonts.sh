affbbb6fa1418fc9d8422bf3957af3e22a481511
Kevin Barabash <kevinb7@gmail.com>
2017-12-29 09:58:27 -0700

    move source files for building fonts into this repo (#6)

--- buildFonts.sh
+++ buildFonts.sh
@@ -6,14 +6,7 @@ usage() {
         echo "$1" >&2
         shift
     done
-    echo "Usage: ${0##*/} [OPTIONS] [SOURCE]"
-    echo ""
-    echo "SOURCE may be"
-    echo "  - a URL for a tarball, or"
-    echo "  - a local tarball file, or"
-    echo "  - a local directory"
-    echo "with a layout compatible to MathJax-dev."
-    echo "It defaults to ${URL}"
+    echo "Usage: ${0##*/} [OPTIONS]"
     echo ""
     echo "OPTIONS:"
     echo "  -h|--help         display this help"
@@ -61,56 +54,15 @@ CONTAINER=
 trap cleanup EXIT
 
 IMAGE="katex/fonts:DF-$(openssl sha1 Dockerfile | tail -c 9)"
-URL=https://github.com/Khan/MathJax-dev/archive/master.tar.gz
-TMPFILE=
-FILE=
-NARGS=0
-while [[ $# -gt 0 ]]; do
-    case "$1" in
-        -h|--help)
-            usage 0
-            ;;
-        --image=*)
-            IMAGE="${1#*=}"
-            ;;
-        --image)
-            shift
-            IMAGE="$1"
-            ;;
-        -*)
-            usage "Invalid option: $1" "" 1
-            ;;
-        *)
-            case $NARGS in
-                0)
-                    if [[ -d "$1" ]]; then
-                        TMPFILE="$(mktemp "${TMPDIR:-/tmp}/mjf.XXXXXXXX")"
-                        FILE="$TMPFILE"
-                        pushd "$1"
-                        if [[ ! -f fonts/OTF/TeX/Makefile ]]; then
-                            echo "$1 does not look like MathJax-dev" >&2
-                            exit 1
-                        fi
-                        tar cf "$FILE" Makefile default.cfg fonts/OTF/TeX
-                        popd
-                    elif [[ -f "$1" ]]; then
-                        FILE="$1"
-                    elif [[ "$1" = http?(s)://* ]]; then
-                        URL="$1"
-                    else
-                        echo "'$1' is not a valid source" >&2
-                        exit 1
-                    fi
-                    NARGS=1
-                    ;;
-                *)
-                    usage "Too many arguments: $1" "" 1
-                    ;;
-            esac
-            ;;
-    esac
-    shift
-done
+TMPFILE="$(mktemp "${TMPDIR:-/tmp}/mjf.XXXXXXXX")"
+FILE="$TMPFILE"
+pushd "src"
+if [[ ! -f fonts/OTF/TeX/Makefile ]]; then
+    echo "src does not look like MathJax-dev" >&2
+    exit 1
+fi
+tar cf "$FILE" Makefile default.cfg fonts/OTF/TeX
+popd
 
 # build image if missing
 if [[ $(docker images "$IMAGE" | wc -l) -lt 2 ]]; then
@@ -119,10 +71,7 @@ if [[ $(docker images "$IMAGE" | wc -l) -lt 2 ]]; then
 fi
 
 CMDS="set -ex
-test -f MathJax-dev.tar.gz || wget -O MathJax-dev.tar.gz '${URL}'
-mk=\$(tar tf MathJax-dev.tar.gz | grep 'fonts/OTF/TeX/Makefile\$')
 tar xf MathJax-dev.tar.gz
-cd \"\${mk%fonts/*}\"
 cp default.cfg custom.cfg
 make custom.cfg.pl
 make -C fonts/OTF/TeX ${filetypes[*]}
@@ -139,10 +88,12 @@ cleanup
 echo "Docker executed successfully, will now unpack the fonts"
 
 tar xf fonts.tar
+mv fonts temp
+mkdir fonts
 for filetype in "${filetypes[@]}"; do
     for font in "${used_fonts[@]}"; do
         echo "$filetype/$font"
-        mv "fonts/OTF/TeX/$filetype/$font".* ./fonts/
+        mv "temp/OTF/TeX/$filetype/$font".* ./fonts/
     done
 done
-rm -rf fonts fonts.tar
+rm -rf temp fonts.tar
